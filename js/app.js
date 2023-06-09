9e1bd44149a8e5f6cf7a055dcde8b8b22eb4b53d
danielntll <dev@danielntll.it>
2023-06-09 18:03:17 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -1,13 +1,14 @@
 import { renderHomePage } from "../Pages/Home_page/homepage.js";
 
 // -------------- INITIALIZZATION --------------------
+export const BASE_PATH = "";
 
 switch (window.location.pathname.split(".")[0]) {
   case "/index":
     renderHomePage();
     break;
   case "/tv-series":
-    renderTvSeries();
+    // renderTvSeries();
     break;
   default:
     renderHomePage();
